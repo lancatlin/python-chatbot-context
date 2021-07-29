c9a2445408d47183a40034d0f63a8b8e53f02bb5
wancat <wancat@wancat.cc>
2021-07-29 11:31:47 +0800

    Finish guess

--- guess/guess.py
+++ guess/guess.py
@@ -1,20 +1,44 @@
-from .message_queue import MessageQueue
+from .message_queue import MessageQueue, RequestTimout
 from .line import reply_text, get_room, get_msg
+from random import randint
 
 
 class Guess:
     def __init__(self, event):
         self.event = event
-        self.guess()
+        try:
+            self.guess()
+        except RequestTimout:
+            self.reply('Timeout')
 
     def guess(self):
-        msg = self.ask('Please input something')
-        self.reply(f'you input {msg}')
+        min_value = self.ask_number('From what number?')
+        max_value = self.ask_number('To what number?')
+        secret = randint(min_value, max_value)
+        msg = f'Guess a number between {min_value} to {max_value}'
+        counter = 0
+        while True:
+            counter += 1
+            answer = self.ask_number(msg)
+            if answer > secret:
+                msg = 'too large'
+            elif answer < secret:
+                msg = 'too small'
+            else:
+                break
+        self.reply(f'You spent {counter} times to guess the secret number.')
 
     def ask(self, *msg):
         self.reply(*msg)
         self.event = MessageQueue.request(get_room(self.event))
         return get_msg(self.event)
+
+    def ask_number(self, *msg):
+        try:
+            content = self.ask(*msg)
+            return int(content)
+        except ValueError:
+            return self.ask_number('Please input an integer.', *msg)
 
     def reply(self, *msg):
         reply_text(self.event, *msg)
---
+++ guess/tests/test_guess.py
@@ -0,0 +1,37 @@
+from time import sleep
+from unittest.mock import patch
+from django.test import TestCase
+from linebot.models.events import MessageEvent, TextMessage
+from linebot.models.sources import SourceUser
+from guess.views import route
+from threading import Thread
+
+
+def sample_msg(msg, user_id=1):
+    return MessageEvent(message=TextMessage(text=msg), source=SourceUser(user_id=user_id))
+
+
+def send(msg, user_id=1):
+    thread = Thread(target=route, args=(sample_msg(msg, user_id), ))
+    thread.start()
+    sleep(0.1)
+
+
+class Guess(TestCase):
+
+    @patch('guess.guess.randint', return_value=63)
+    @patch('guess.guess.Guess.reply')
+    def test_guess(self, mock_reply, mock_random):
+        send('/guess')
+        mock_reply.assert_called_with('From what number?')
+        send('10')
+        mock_reply.assert_called_with('To what number?')
+        send('100')
+        mock_reply.assert_called_with(f'Guess a number between 10 to 100')
+        send('34')
+        mock_reply.assert_called_with('too small')
+        send('72')
+        mock_reply.assert_called_with('too large')
+        send('63')
+        mock_reply.assert_called_with(
+            f'You spent 3 times to guess the secret number.')
